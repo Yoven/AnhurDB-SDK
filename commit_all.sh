333b7e2dccfe32355f8c854a737d316d0cd93eff
Paulo Junior <paulo.junior@yoven.io>
2026-03-13 19:21:12 +0000

    feat: moving scripts...

--- commit_all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-export PYTHONUNBUFFERED=1
-git add v2/python/tests/test_crypto.py
-git add .github/workflows/ci.yml
-git commit -m "ci: split tests and build steps into 4 parallel jobs and fix python test mock"
-git push
