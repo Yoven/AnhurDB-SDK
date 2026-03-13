37f325d5391a21307460ac2a10750c24d71fec95
Paulo Junior <paulo.junior@yoven.io>
2026-03-13 19:02:02 +0000

    ci: split tests and build steps into 4 parallel jobs and fix python test mock

--- v2/python/tests/test_crypto.py
+++ v2/python/tests/test_crypto.py
@@ -8,9 +8,9 @@ from anhurdb.crypto.quantizer import cosine_similarity
 
 class TestCrypto(unittest.TestCase):
     def test_cosine_similarity(self):
-        # Just a stub test for now
-        res = cosine_similarity([1.0], [1.0])
-        self.assertIsNone(res)
+        # Stub test validating the lack of implementation locally
+        with self.assertRaises(NotImplementedError):
+            cosine_similarity([1.0], [1.0])
 
 if __name__ == '__main__':
     unittest.main()
